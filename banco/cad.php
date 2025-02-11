67310d6d48a2fb18f5ff3a4ff2e1dd449ea6246b
Henrique Akira Murakami <henriquemurakami14@hotmail.com>
2025-02-11 11:21:11 -0300

    Update cad.php

--- banco/cad.php
+++ banco/cad.php
@@ -3,9 +3,9 @@
     $contas = 0; 
     
     do {
-        $criar_conta = readline("[1] Criar Conta   [2] Remover Conta   Selecione a opção: ");
+        $criar_conta = readline("[1] Criar Conta   [2] Remover Conta    [3] Depositar   Selecione a opção: ");
     
-        if ($criar_conta == "1" || $criar_conta == "2") {
+        if ($criar_conta == "1" || $criar_conta == "2" || $criar_conta == "3") {
             $validado = true;
         } else {
             $validado = false;
@@ -19,10 +19,14 @@
         clientes($cliente, $contas);
     }elseif($criar_conta == "2") {
         remover_conta();
+    }elseif($criar_conta == "3") {
+        depositar();
     }
     
 
     function cadastro(&$cliente, $contas){
+        $clientes = json_decode(file_get_contents("clientes.json"), true);
+
 
         $cliente[$contas]["nome"] = strtoupper(readline("Digite seu nome: "));
         
@@ -30,21 +34,18 @@
         do {
             $cpf = readline("Digite seu cpf: ");
             
-            validaCPF($cpf);
+            validaCPF($cpf, $clientes);
 
-            if (validaCPF($cpf)) {
+            if (validaCPF($cpf, $clientes)) {
                 $cliente[$contas]["cpf"] = $cpf;
             }else {
                 echo "CPF inválido! ";
             }
-        } while (!validaCPF($cpf));
+        } while (!validaCPF($cpf, $clientes));
         
-
 
         do {
             $telefone = readline("Digite seu número de telefone: ");
-
-            valida_telefone($telefone);
 
             if (valida_telefone($telefone)){
                 $cliente[$contas]["telefone"] = $telefone;
@@ -54,11 +55,36 @@
 
         } while (!valida_telefone($telefone));
         
-        $cliente[$contas]["senha"] = readline("Crie sua senha:");
 
+        do {
+            $senha = readline("Crie sua senha:");
+
+            if (valida_senha($senha)) {
+                $cliente[$contas]["senha"] = $senha;
+            } else {
+                echo "Senha Inválida! As senhas devem conter, maiúsculas, minúsculas, números, caracteres especiais. ";
+            }
+
+        } while (!valida_senha($senha));
+
+        $cliente[$contas]["saldo"] = 0;
+
+        $json = json_encode($clientes, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+        file_put_contents("clientes.json", $json);
     }
 
-    function validaCPF(&$cpf) {
+
+
+    function validaCPF(&$cpf, $clientes) {
+        $clientes = json_decode(file_get_contents("clientes.json"), true);
+
+        $cpf = preg_replace("/(\d{3})(\d{3})(\d{3})(\d{2})/", "$1.$2.$3-$4", $cpf);
+
+        for ($i=0; $i < count($clientes); $i++) { 
+            if ($cpf == $clientes[$i]["cpf"]) {
+                return false;
+            }
+        }
 
         // Extrai somente os números
         $cpf = preg_replace( '/[^0-9]/is', '', $cpf );
@@ -88,7 +114,10 @@
         return true;
     }
 
+
+
     function valida_telefone(&$telefone) {
+
         // Remove caracteres não numéricos
         $telefone = preg_replace('/\D/', '', $telefone);
     
@@ -107,8 +136,36 @@
     
         return true;
     }
-    function valida_senha(){
 
+
+
+    function valida_senha($senha){
+        // Verifica se a senha tem pelo menos 8 caracteres
+        if (strlen($senha) < 8) {
+            return false;
+        }
+
+        // Verifica se tem pelo menos uma letra maiúscula
+        if (!preg_match('/[A-Z]/', $senha)) {
+            return false;
+        }
+
+        // Verifica se tem pelo menos uma letra minúscula
+        if (!preg_match('/[a-z]/', $senha)) {
+            return false;
+        }
+
+        // Verifica se tem pelo menos um número
+        if (!preg_match('/[0-9]/', $senha)) {
+            return false;
+        }
+
+        // Verifica se tem pelo menos um caractere especial ($, @, #, etc.)
+        if (!preg_match('/[\W]/', $senha)) {
+            return false;
+        }
+
+        return true;         
     }
     
     function remover_conta(){
@@ -122,7 +179,7 @@
             
         $cpf = readline("digite seu cpf: ");
         
-        validaCPF($cpf, $clientes, $quantidades_contas);
+        validaCPF($cpf, $clientes);
 
     
         for ($i = 0; $i < $quantidades_contas ; $i++) { 
@@ -165,4 +222,58 @@
 
     }
 
+    function depositar(){
+        $clientes = json_decode(file_get_contents("clientes.json"), true);
+        $cpf_encontrado = false;
+        $senha_encontrada = false;
+        $valor_validado = false;
+        $quantidades_contas = count($clientes);
+        
+        do {
+            
+        $cpf = readline("digite seu cpf: ");
+        
+        validaCPF($cpf, $clientes);
+
+    
+        for ($i = 0; $i < $quantidades_contas ; $i++) { 
+
+                if ($cpf == $clientes[$i]["cpf"]){
+
+                $cpf_encontrado = true;
+
+                    do {
+                        $senha = readline("digite sua senha: ");
+
+                        if ($senha == $clientes[$i]["senha"]) {
+                            $senha_encontrada = true;
+
+                                do {
+                                    
+                                    $deposito = readline("digite o valor do depósito: ");
+                                    $clientes[$i]["saldo"] += $deposito;
+                                    $valor_validado = true;
+                                } while ($valor_validado == false);
+
+                        }else {
+                            print("Você errou sua senha. ");
+                        }
+
+                    } while ($senha_encontrada == false);
+                }
+
+                }if ($cpf_encontrado == false) {
+                    print "CPF  inválido! ";
+                }
+        
+        }while($cpf_encontrado != true && $senha_encontrada != true);
+
+
+        $json = json_encode($clientes, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+        file_put_contents("clientes.json", $json);
+    }
+
+
+
+    
 ?>
